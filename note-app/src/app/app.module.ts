c75fa84856ed00fa9a5050719f7181e2ba755902
José Alexandre da Silva França <alexandrefranca208@gmail.com>
2023-11-15 19:14:01 -0300

    Marcações

---
+++ note-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewNoteComponent } from './new-note/new-note.component';
+import { NoteComponent } from './note/note.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewNoteComponent,
+    NoteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
